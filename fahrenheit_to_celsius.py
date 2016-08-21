f8fa30387a31e1e9b64ca0a3fd8f59086da1b4c4
DKMonroe <david.dmonroe@gmail.com>
2016-08-21 14:21:42 -0400

    Validate user input
    If the user enters something other than a float, it let's the user know what to do.

--- fahrenheit_to_celsius.py
+++ fahrenheit_to_celsius.py
@@ -1,3 +1,7 @@
 fahrenheit = int(raw_input('Enter the temperature in Fahrenheit and I will convert it to Celsius: '))
-celsius = (fahrenheit - 32) * 5 / 9
-print "It is ", celsius, "degrees Celsius!"
+try:
+	fahrenheit = float(inp)
+	celsius = (fahrenheit - 32) * 5 / 9
+	print "It is ", celsius, "degrees Celsius!"
+except:
+	print 'Please enter a number'
