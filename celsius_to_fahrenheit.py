dee7cdf1f47a9e2eac7abbe3a0d83ce8b711fee2
DKMonroe <david.dmonroe@gmail.com>
2016-08-21 09:42:15 -0400

    Utilities for celsius and fahrenheit
    Transforms one or the other.

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,3 @@
+celsius = int(raw_input('Enter the temperature in Celsius and I will convert it to Fahrenheit: '))
+fahrenheit = celsius * 9 / 5 + 32
+print "It is ", fahrenheit, "degrees Fahrenheit!"
